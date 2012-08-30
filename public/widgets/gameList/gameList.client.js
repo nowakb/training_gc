0e554bc107e55d3f9d3e86a74d0def3e51d5d9ee
Annie Bougie <anne.bougie@gmail.com>
2012-08-30 16:13:07 -0500

    Create random data source mock for game numbers and stats

--- public/widgets/gameList/gameList.client.js
+++ public/widgets/gameList/gameList.client.js
@@ -1,10 +1,23 @@
-var games = [ 
-    {guid: 0, name: "Ninja", currentGames: 81, numberOfPlayers: 1, currentlyWaiting: 0},
-    {guid: 1, name: "Hamburger", currentGames: 2, numberOfPlayers: 4, currentlyWaiting: 3},
-    {guid: 2, name: "Chess", currentGames: 43, numberOfPlayers: 2, currentlyWaiting: 1}
+var masterGames = [ 
+    {name: "Ninja", numberOfPlayers: 1},
+    {name: "Hamburger", numberOfPlayers: 4}, 
+    {name: "Chess", numberOfPlayers: 2},
+    {name: "Monster Bowling", numberOfPlayers: 4},
+    {name: "End Runner", numberOfPlayers: 8},
+    {name: "Monitor Throwing", numberOfPlayers: 10},
+    {name: "Grocery Bagger", numberOfPlayers: 3},
+    {name: "Bug Squash", numberOfPlayers: 5},
+    {name: "Sally Says", numberOfPlayers: 10},
+    {name: "Alien Masher", numberOfPlayers: 2},
+    {name: "Magic", numberOfPlayers: 6}
     ];
 
-var newGame = {guid: 3, name: "Magic", currentGames: 1, numberOfPlayers: 2, currentlyWaiting: 1};
+    // {guid: 0, name: "Ninja", currentGames: 81, numberOfPlayers: 1, currentlyWaiting: 0},
+    // {guid: 1, name: "Hamburger", currentGames: 2, numberOfPlayers: 4, currentlyWaiting: 3},
+    // {guid: 2, name: "Chess", currentGames: 43, numberOfPlayers: 2, currentlyWaiting: 1}
+    
+
+
 
 feather.ns("training_gc");
 (function() {
@@ -13,7 +26,8 @@ feather.ns("training_gc");
     path: "widgets/gameList/",
     prototype: {
       onInit: function() {
-        
+        var me = this;
+        me.games = getMockGameData(me.games);
       },
       onReady: function() {
         var me = this;
@@ -50,3 +64,50 @@ feather.ns("training_gc");
     }
   });
 })();
+
+function getMockGameData(games) {
+  debugger;
+  if (games == undefined)
+    games = [];
+  
+  var newAddCount = randomFromTo(2, 8);
+
+  // add up new games
+  for (var j = 0; j < newAddCount; j++) {
+    
+    var freeIndex = false;
+    do {
+      var k = randomFromTo(0, masterGames.length - 1); 
+      var masterGame = masterGames[k];
+      freeIndex = (games.length == 0);
+
+      for (var l = 0; l < games.length; l++) {
+        if (masterGame.name != games[l].name) {
+          freeIndex = true;
+          break;
+        }
+      }
+    } while (!freeIndex)
+
+    var game = masterGames[k];
+    game.currentGames = getCurrentGamesCount(0);
+    game.currentlyWaiting = getCurrentlyPlaying(game.numberOfPlayers);
+    games.push(game);
+  }
+
+  return games;
+}
+
+function getCurrentlyPlaying(numberOfPlayers) {
+  return randomFromTo(1, numberOfPlayers - 1);
+}
+
+function getCurrentGamesCount(starting) {
+  var add = randomFromTo(0, 20);
+  var remove = randomFromTo(0, 10);
+  return starting + add - remove;
+}
+
+function randomFromTo(from, to){
+  return Math.floor(Math.random() * (to - from + 1) + from);
+}
