d7d014bc2a15fff549e0c02a22215b5a73074cf2
Annie Bougie <anne.bougie@gmail.com>
2012-08-31 11:49:00 -0500

    get mocking working

--- public/widgets/gameList/gameList.client.js
+++ public/widgets/gameList/gameList.client.js
@@ -9,17 +9,11 @@ var masterGames = [
     {name: "Bug Squash", numberOfPlayers: 5},
     {name: "Sally Says", numberOfPlayers: 10},
     {name: "Alien Masher", numberOfPlayers: 2},
-    {name: "Magic", numberOfPlayers: 6}
+    {name: "Magic", numberOfPlayers: 6},
+    {name: "Fruit Ninja", numberOfPlayers:  4},
+    {name: "Dance Party", numberOfPlayers: 8},
+    {name: "Sing Snap", numberOfPlayers: 25}
     ];
-
-    // {guid: 0, name: "Ninja", currentGames: 81, numberOfPlayers: 1, currentlyWaiting: 0},
-    // {guid: 1, name: "Hamburger", currentGames: 2, numberOfPlayers: 4, currentlyWaiting: 3},
-    // {guid: 2, name: "Chess", currentGames: 43, numberOfPlayers: 2, currentlyWaiting: 1}
-    
-
-
-
-var updateGame = {guid: 1, name: "Hamburger", currentGames: 3, numberOfPlayers: 5, currentlyWaiting: 4};
 
 feather.ns("training_gc");
 (function() {
@@ -55,21 +49,37 @@ feather.ns("training_gc");
           var gameLine = me.children && me.children.findById("gameLine" + g.guid);
           if(gameLine) {
             gameLine.updateData(g);
+            if (g.DeleteMe) {
+              gameLine.dispose();
+              var newGames = [];
+              for (var j = 0; j < me.games.length; j++) {
+                if (!me.games[j].DeleteMe)
+                  newGames.push(me.games[i])
+              }
+              me.games = newGames;
+            }
           } else {
             appendGameLine(g);
           } 
         }
-               
-        for(var i=0; i<games.length; i++) {
-          updateGameLine(games[i]);
+        
+        //debugger;       
+        for(var i=0; i<me.games.length; i++) {
+          updateGameLine(me.games[i]);
         }
         
         me.domEvents.bind(me.get("#addnewgame"), 'click', function() {
-          updateGameLine(newGame);
+          addNewGames(me.games, 1);
+          updateGameLine(me.games[me.games.length - 1]);
         });
         
         me.domEvents.bind(me.get("#updategame"), 'click', function() {
-          updateGameLine(updateGame);
+          getMockGameData(me.games, 0)
+          debugger;
+          for(i = 0; i < me.games.length; i++) {
+            var game = me.games[i];
+            updateGameLine(game);
+          }
         });
         
       }
@@ -77,34 +87,48 @@ feather.ns("training_gc");
   });
 })();
 
-function getMockGameData(games) {
+function getMockGameData(games, addCount) {
   debugger;
   if (games == undefined)
     games = [];
   
-  var newAddCount = randomFromTo(2, 8);
+  if (addCount == undefined)
+    addCount = randomFromTo(2, 4);
+  if (games.length + addCount == 11 && addCount > 0) 
+    alert("you can't add any more games.");
 
-  // add up new games
-  for (var j = 0; j < newAddCount; j++) {
-    
-    var freeIndex = false;
-    do {
-      var k = randomFromTo(0, masterGames.length - 1); 
-      var masterGame = masterGames[k];
-      freeIndex = (games.length == 0);
+  games = addNewGames(games, addCount);
 
-      for (var l = 0; l < games.length; l++) {
-        if (masterGame.name != games[l].name) {
-          freeIndex = true;
-          break;
-        }
-      }
-    } while (!freeIndex)
+  if (addCount == 0) {
+    debugger;
+    var countAdd = randomFromTo(1, 2);
+    var countRemove = randomFromTo(1, 2);
+    if ((countAdd + games.length - 1) > 11) 
+      countAdd = 0;
+     if ((games.length - countRemove + countAdd) < 1)
+       countRemove = 0;
 
-    var game = masterGames[k];
-    game.currentGames = getCurrentGamesCount(0);
-    game.currentlyWaiting = getCurrentlyPlaying(game.numberOfPlayers);
-    games.push(game);
+    countAdd = 0;
+    //countRemove = 0;
+    var i;
+
+    // update stats on current games
+    for (i = 0; i < games.length; i++) {
+      var g = games[i];
+      var waiting = getCurrentlyPlaying(g.numberOfPlayers + 1);
+      if (waiting == g.numberOfPlayers)
+        g.DeleteMe = true;
+      g.currentlyWaiting = waiting;
+    }
+
+    //     // remove games
+    //  for (i = 0; i < countRemove; i++) {
+    //    var k = randomFromTo(0, games.length - 1);
+    //    games[k].DeleteMe = true;
+    // }
+
+    // add new games
+    addNewGames(games, countAdd);
   }
 
   return games;
@@ -114,12 +138,34 @@ function getCurrentlyPlaying(numberOfPlayers) {
   return randomFromTo(1, numberOfPlayers - 1);
 }
 
-function getCurrentGamesCount(starting) {
-  var add = randomFromTo(0, 20);
-  var remove = randomFromTo(0, 10);
-  return starting + add - remove;
-}
-
 function randomFromTo(from, to){
   return Math.floor(Math.random() * (to - from + 1) + from);
+}
+
+function addNewGames(games, addCount) {
+  // add up new games
+  for (var j = 0; j < addCount; j++) {
+    do {
+      var freeIndex = true;
+      var k = randomFromTo(0, masterGames.length - 1); 
+      var masterGame = masterGames[k];
+
+      for (var l = 0; l < games.length; l++) {
+        if (masterGame.name == games[l].name) {
+          freeIndex = false;
+          break;
+        }
+      }
+    } while (!freeIndex)
+
+    var game = masterGames[k];
+    game.guid = k;
+    var waiting = getCurrentlyPlaying(game.numberOfPlayers);
+
+    game.currentlyWaiting = waiting;
+    if (waiting < game.numberOfPlayers)
+      games.push(game);
+  }
+
+  return games;
 }
